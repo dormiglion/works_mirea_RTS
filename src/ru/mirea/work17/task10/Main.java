d21921d3be5150a39a0373dd6facac4308d7df99
dormiglion <lavrenty.ozerov@yandex.ru>
2024-11-21 22:01:41 +0300

    add 17!

---
+++ src/ru/mirea/work17/task10/Main.java
@@ -0,0 +1,21 @@
+package ru.mirea.work17.task10;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.add("banana");
+        list.add("apple");
+        list.add("cherry");
+        list.add("date");
+        list.add("hay");
+
+        System.out.println("Исходный список:");
+        list.printList();
+
+        list.moveMaxToFront();
+
+        System.out.println("Список после перемещения максимального элемента в начало:");
+        list.printList();
+    }
+}
---
+++ src/ru/mirea/work17/task9/Main.java
@@ -0,0 +1,19 @@
+package ru.mirea.work17.task9;
+
+public class Main {
+    public static void main(String[] args) {
+        SortedLinkedList list = new SortedLinkedList();
+
+        list.add("banana");
+        list.add("apple");
+        list.add("cherry");
+        list.add("date");
+
+        System.out.println("Список после вставки:");
+        list.printList();
+
+        list.add("apricot");
+        System.out.println("Список после вставки 'apricot':");
+        list.printList();
+    }
+}
